42de78202b4c8f03710aecd0d5a12b015dc46c7d
mealuc <aluc_78@hotmail.com>
2021-05-01 19:48:00 +0300

    coordinate adding system were added

--- components/Articles.js
+++ components/Articles.js
@@ -4,7 +4,7 @@ import * as firebase from 'firebase';
 import { SearchBar } from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Geolocation from '@react-native-community/geolocation';
-import MapView, { PROVIDER_GOOGLE,Marker,AnimatedRegion } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, AnimatedRegion } from 'react-native-maps';
 class Articles extends Component {
   constructor() {
     super();
@@ -26,36 +26,50 @@ class Articles extends Component {
       'message': 'Konumu Ver!'
     });
     //alert(response);
-    /*Geolocation.getCurrentPosition(
-      (position) => {      
-        let { coords: { latitude, longitude } } = position;
-        this.setState({ latitude, longitude });
-        console.log(position)
-      },
-      (error) => {
-        console.log(error.code, error.message);
-      },
-      { enableHighAccuracy: true, timeout: 30000}
-    );*/
+    
     const useruid = firebase.auth().currentUser.uid;
     firebase.database().ref('users/' + useruid).once('value')
       .then(snapshot => {
         const data = snapshot.val();
         this.setState({ name: data.name, email: data.email });
       });
-      Geolocation.watchPosition(
-        position =>{
-          const { coords : {latitude,longitude}} = position;
-          this.setState({latitude,longitude})         
-        },
-        error =>{
-          console.log(error);
-        },
-        {enableHighAccuracy:true}
-      );
+    Geolocation.watchPosition(
+      position => {
+        const { coords: { latitude, longitude } } = position;
+        this.setState({ latitude, longitude })
+      },
+      error => {
+        console.log(error);
+      },
+      { enableHighAccuracy: true }
+    );
+  }
+find_myposition=()=>{
+  Geolocation.getCurrentPosition(
+    (position) => {      
+      let { coords: { latitude, longitude } } = position;
+      this.setState({ latitude, longitude });
+    },
+    (error) => {
+      console.log(error.code, error.message);
+    },
+    { enableHighAccuracy: true, timeout: 30000}
+  );
+}
+  
+
+  save_coords = () => {
+    var database = firebase.database().ref('coords/');
+    database.push({
+      latitude: this.state.latitude,
+      longitude: this.state.longitude,
+      owner_email:this.state.email,
+      owner_name:this.state.name,
+    }).then((result) => console.log(result))
+      .catch((error) => console.log(error));
   }
 
-  stop_observing =() =>{
+  stop_observing = () => {
     Geolocation.stopObserving();
   };
 
@@ -74,45 +88,52 @@ class Articles extends Component {
         <Text>
           longitude:{longitude}
         </Text>
-        <View style={styles.articleContainer}>         
+        <View style={styles.articleContainer}>
           <View style={styles.mapcontainer}>
             <MapView
               showsMyLocationButton={true}
               provider={PROVIDER_GOOGLE}///////// remove if not using Google Maps
               style={styles.map}
               region={{
-                latitude:  Number(41.2351403),
+                latitude: Number(41.2351403),
                 longitude: Number(32.6669429),
                 latitudeDelta: 0.015,
                 longitudeDelta: 0.0121,
-              }}             
+              }}
               followsUserLocation={true}
               showsUserLocation={true}
             >
               <Marker
-              draggable={true}              
-              onDragEnd={(e)=>console.log(e.nativeEvent.coordinate)}
-              title={"Here"}
-              description={"MyHome"}
-              coordinate={{
-                latitude: 41.2351403,
-                longitude: 32.6669429,
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-              }}/>
+                draggable={true}
+                onDragEnd={(e) =>
+                  this.setState({ latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude })}
+                title={"Here"}
+                description={"MyHome"}
+                coordinate={{
+                  latitude: 41.2351403,
+                  longitude: 32.6669429,
+                  latitudeDelta: 0.015,
+                  longitudeDelta: 0.0121,
+                }} />
             </MapView>
           </View>
           <Text style={styles.content}>
             Hi {email}/{name}
           </Text>
+          <TouchableOpacity style={{ padding: 20, width: 150 }} onPress={this.save_coords}>
+            <Text style={{ color: '#1B9CFC' }}>Konumu Kaydet</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ padding: 20, width: 150 }} onPress={this.find_myposition}>
+            <Text style={{ color: '#1B9CFC' }}>Konumumu Bul</Text>
+          </TouchableOpacity>
           <TouchableOpacity style={{ padding: 20, width: 85 }} onPress={() => firebase.auth().signOut()} >
             <Text style={{ color: '#1B9CFC' }}>
               Logout
         </Text>
           </TouchableOpacity>
           <TouchableOpacity style={{ padding: 20, width: 85 }} onPress={this.stop_observing}>
-          <Text style={{ color: '#1B9CFC' }}>Durdur</Text>
-        </TouchableOpacity>
+            <Text style={{ color: '#1B9CFC' }}>Durdur</Text>
+          </TouchableOpacity>
         </View>
       </View>
     )
@@ -133,21 +154,21 @@ const styles = StyleSheet.create({
     fontSize: 19
   },
   searchbar: {
-    padding:10,
+    padding: 10,
     justifyContent: 'center',
     alignItems: 'center',
     width: 10,
     fontSize: 16,
   },
   mapcontainer: {
-    flex:1,
+    flex: 1,
     ...StyleSheet.absoluteFillObject,
     height: 400,
     width: 400,
 
   },
   map: {
-    flex:1,
+    flex: 1,
     ...StyleSheet.absoluteFillObject,
   },
 });
